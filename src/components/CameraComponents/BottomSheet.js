3281023df11e5bbe7846e65adcd473373e6d2bf4
Haibert Barfian <haibertdude@gmail.com>
2021-06-04 18:57:47 -0700

    a lot

---
+++ src/components/CameraComponents/BottomSheet.js
@@ -0,0 +1,292 @@
+import React, {
+    useCallback,
+    useRef,
+    useMemo,
+    useImperativeHandle,
+    forwardRef,
+    useState,
+} from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
+import {
+    BottomSheetModal,
+    BottomSheetModalProvider,
+    BottomSheetScrollView,
+    BottomSheetFlatList,
+    BottomSheetView,
+} from '@gorhom/bottom-sheet'
+// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import Animated, {
+    Extrapolate,
+    interpolate,
+    useAnimatedStyle,
+} from 'react-native-reanimated'
+
+import {
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+} from '@gorhom/bottom-sheet'
+
+//Due to wrapping the content and handle with TapGestureHandler & PanGestureHandler, any gesture interaction would not function as expected.
+//To resolve this issue, please use touchables that this library provides.
+
+//custom CameraComponents
+import Thumbnail from '../../components/Thumbnail'
+import SelectionCellItem from '../../components/CameraComponents/SelectionCellItem'
+
+//safe area
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+//ionicons
+import { Ionicons } from '@expo/vector-icons'
+import { Icon } from 'react-native-elements'
+
+//colors
+import colors from '../../constants/colors'
+
+//redux
+import { addToGallery } from '../../store/camera/actions'
+import { useDispatch, useSelector } from 'react-redux'
+
+const CustomBackdrop = ({ animatedIndex, style }) => {
+    // animated variables
+    const containerAnimatedStyle = useAnimatedStyle(() => ({
+        opacity: interpolate(
+            animatedIndex.value,
+            [0, 1],
+            [0, 1],
+            Extrapolate.CLAMP
+        ),
+    }))
+    // animated styles
+    const containerStyle = useMemo(
+        () => [
+            style,
+            {
+                backgroundColor: 'rgba(94,94,94,0.64)',
+            },
+            containerAnimatedStyle,
+        ],
+        [style, containerAnimatedStyle]
+    )
+
+    return <Animated.View style={containerStyle} />
+}
+
+const CustomHandleComponent = () => {
+    return <View style={{ borderRadius: 20, width: '100%', height: 15 }}></View>
+}
+const BottomSheet = forwardRef((props, ref) => {
+    const dispatch = useDispatch()
+    const insets = useSafeAreaInsets()
+
+    // hooks
+    const bottomSheetModalRef = useRef()
+    const snapPoints = useMemo(() => ['100%', '100%'], [])
+    // callbacks
+    useImperativeHandle(ref, () => ({
+        handlePresentModalPress: () => {
+            bottomSheetModalRef.current?.present()
+        },
+    }))
+    const handleSheetChangesModal = useCallback((index) => {}, [])
+
+    const selectedGalleries = []
+
+    //----------------------------------------------------------------FLAT LIST DATA----------------------------------------------------------------
+    const galleries = useSelector((state) => state.galleryReducer.galleries)
+
+    const renderItem = useCallback(
+        (item, index) => (
+            <SelectionCellItem
+                key={item.galleryID}
+                navigation={props.navigation}
+                galleryName={item.galleryName}
+                galleryID={item.galleryID}
+                onSelect={() => {
+                    const exists = selectedGalleries.includes(item.galleryID)
+                    if (exists) {
+                        const index = selectedGalleries.indexOf(item.galleryID)
+                        if (index > -1) {
+                            selectedGalleries.splice(index, 1)
+                        }
+                    } else {
+                        selectedGalleries.push(item.galleryID)
+                    }
+                }}
+            />
+        ),
+        []
+    )
+
+    //----------------------------------------------------------------FLAT LIST DATA----------------------------------------------------------------
+
+    //----------------------------------------------------------------UPLOADING PICTURE----------------------------------------------------------------
+    const picTaken = useSelector((state) => state.cameraReducer.pictureBase64)
+    const uploadPhotoHandler = useCallback(async () => {
+        if (selectedGalleries.length === 0) return
+        try {
+            await dispatch(
+                addToGallery(
+                    `data:image/jpeg;base64,${picTaken}`,
+                    selectedGalleries
+                )
+            )
+            bottomSheetModalRef.current?.close()
+            props.dismissModal()
+        } catch (err) {}
+    }, [uploadPhotoHandler])
+    //----------------------------------------------------------------UPLOADING PICTURE----------------------------------------------------------------
+
+    return (
+        <BottomSheetModalProvider>
+            <BottomSheetModal
+                ref={bottomSheetModalRef}
+                index={1}
+                snapPoints={snapPoints}
+                onChange={handleSheetChangesModal}
+                backdropComponent={CustomBackdrop}
+                dismissOnPanDown={true}
+                handleComponent={CustomHandleComponent}
+            >
+                <View
+                    style={{
+                        paddingTop: insets.top,
+                        flex: 1,
+                        paddingVertical: 10,
+                    }}
+                >
+                    <BottomSheetScrollView
+                        contentContainerStyle={{
+                            paddingTop: insets.top,
+                            paddingBottom: 50 + insets.bottom,
+                        }}
+                    >
+                        <View style={styles.shadowView}>
+                            <View
+                                style={{ overflow: 'hidden', borderRadius: 20 }}
+                            >
+                                {galleries.map(renderItem)}
+                                {/* <BottomSheetFlatList
+                                style={styles.flatList}
+                                data={galleries}
+                                keyExtractor={(item) => `${item.galleryID}`}
+                                renderItem={({ item, index }) => {
+                    console.log("🚀 ~ file: BottomSheet.js ~ line 144 ~ BottomSheet ~                     const exists = selectedGalleries.find(gallery => gallery.galleryID)
+",                     const exists = selectedGalleries.find(gallery => gallery.galleryID)
+)
+                                    return (
+                                        <SelectionCellItem
+                                            navigation={props.navigation}
+                                            galleryName={item.galleryName}
+                                            galleryID={item.galleryID}
+                                            onSelect={() => {
+                                                selectedGalleries.push(
+                                                    item.galleryID
+                                                )
+                                                console.log(
+                                                    '🚀 ~ file: BottomSheet.js ~ line 88 ~ BottomSheet ~ selectedGalleries',
+                                                    selectedGalleries
+                                                )
+                                            }}
+                                        />
+                                    )
+                                }}
+                                scrollEnabled={false}
+                                showsVerticalScrollIndicator={false}
+                                // columnWrapperStyle={{
+                                //     marginLeft: 10,
+                                // }}
+                                // contentContainerStyle={{
+                                //     paddingBottom: tabBarBottomPosition + 60,
+                                // }}
+                            /> */}
+                                {/* <BottomSheetScrollView
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={styles.contentContainer}
+                    keyboardDismissMode="on-drag"
+                ></BottomSheetScrollView> */}
+                            </View>
+                        </View>
+                    </BottomSheetScrollView>
+                    <View
+                        style={{
+                            ...styles.bottomActions,
+                            height: 50 + insets.bottom,
+                        }}
+                    >
+                        <View style={styles.bottomButtonsCont}>
+                            {/* <View style={styles.circle}>
+                                <Ionicons
+                                    name="download-outline"
+                                    size={28}
+                                    color="white"
+                                    onPress={props.onSave}
+                                />
+                            </View> */}
+                            <TouchableOpacity onPress={uploadPhotoHandler}>
+                                <View style={styles.circle}>
+                                    <Ionicons
+                                        name="send"
+                                        size={28}
+                                        color={colors.lightTint}
+                                        onPress={uploadPhotoHandler}
+                                    />
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </BottomSheetModal>
+        </BottomSheetModalProvider>
+    )
+})
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    flatList: {
+        flex: 1,
+    },
+    shadowView: {
+        flex: 1,
+        shadowColor: 'black',
+        shadowRadius: 7,
+        shadowOpacity: 0.1,
+        shadowOffset: {
+            width: 0,
+            height: 0,
+        },
+        borderRadius: 20,
+        elevation: 5,
+        marginHorizontal: 10,
+        backgroundColor: 'white',
+    },
+    bottomActions: {
+        height: 50,
+        minWidth: '100%',
+        backgroundColor: colors.lightTint,
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        left: 0,
+    },
+    bottomButtonsCont: {
+        minWidth: '100%',
+        justifyContent: 'flex-end',
+        flexDirection: 'row',
+        padding: 10,
+    },
+
+    circle: {
+        height: 40,
+        width: 60,
+        borderRadius: 20,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+export default BottomSheet
